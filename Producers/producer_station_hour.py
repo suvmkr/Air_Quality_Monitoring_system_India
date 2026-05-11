c2c96d396a037965f1af84d74140dbfce0d25f78
Shubham kumar <125624127+suvmkr@users.noreply.github.com>
2026-05-11 21:34:20 +0530

    Add files via upload

---
+++ Producers/producer_station_hour.py
@@ -0,0 +1,48 @@
+from confluent_kafka import Producer
+import json
+import time
+import pandas as pd
+import numpy as np
+
+conf = {
+    'bootstrap.servers': 'capstroneventhub.servicebus.windows.net:9093',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': '$ConnectionString',
+    'sasl.password': '****',
+    'client.id': 'hour-station-producer'
+}
+
+producer = Producer(conf)
+
+def stream_data(file_path, interval, data_type):
+    df = pd.read_csv(file_path)
+    time_col = 'Datetime' if 'Datetime' in df.columns else 'Date'
+    df[time_col] = pd.to_datetime(df[time_col])
+    df = df[df[time_col].dt.year == 2020]
+    if df.empty:
+        print(f"No data found for year 2020 in {file_path}. Exiting.")
+        return
+    df = df.sort_values(by=time_col)
+    grouped = df.groupby(time_col)
+    print(f"Starting {data_type} producer...")
+
+    for timestamp, group in grouped:
+        for _, row in group.iterrows():
+            data = row.replace({np.nan: None}).to_dict()
+            data[time_col] = str(data[time_col])
+            data['origin_type'] = data_type 
+            producer.produce('aqiproject', value=json.dumps(data))
+        producer.flush()
+        print(f"Sent {len(group)} records for {timestamp}.")
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    FILE_NAME = 'data/station_hour.csv'
+    INTERVAL = 5
+    TYPE = 'STATION_HOUR'
+    
+    try:
+        stream_data(FILE_NAME, INTERVAL, TYPE)
+    except KeyboardInterrupt:
+        print("\nStopping Producer...")
